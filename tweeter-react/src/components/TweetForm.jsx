1c5b97c3002bd956762b3bb8beafba8e71a3e161
Francis Bourgouin <netbeuix@gmail.com>
2021-08-04 17:12:47 -0400

    fixed small value mistake

--- tweeter-react/src/components/TweetForm.jsx
+++ tweeter-react/src/components/TweetForm.jsx
@@ -14,8 +14,8 @@ export default function TweetForm(props) {
   return (
     <section className="newtweet">
       <form onSubmit={submitTweet} className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
-        <input type="submit" value="Tweet" className="form__input" onChange={event => setTweetText(event.target.value)} />
+        <textarea className="form__textarea" name="text" placeholder="What are you humming about?" value={tweetText} onChange={event => setTweetText(event.target.value)} ></textarea>
+        <input type="submit" value="Tweet" className="form__input" />
         <span style={spanStyle} className="form__counter">{tweetRemainingLength}</span>
       </form>
     </section>
