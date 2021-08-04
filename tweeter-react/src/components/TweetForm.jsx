24c05000c0277f01768cd8086c623765cd6db8a4
Francis Bourgouin <netbeuix@gmail.com>
2021-08-04 17:05:25 -0400

    added controlled form

--- tweeter-react/src/components/TweetForm.jsx
+++ tweeter-react/src/components/TweetForm.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import "./TweetForm.css"
 
 export default function TweetForm(props) {
+  const [tweetText, setTweetText] = useState("")
+
+  const tweetRemainingLength = 140 - tweetText.length
+  const spanStyle = { color: tweetRemainingLength >= 0 ? "black" : "red" }
+
+  const submitTweet = event => {
+    event.preventDefault()
+  }
 
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form onSubmit={submitTweet} className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
-        <span className="form__counter">140</span>
+        <input type="submit" value="Tweet" className="form__input" onChange={event => setTweetText(event.target.value)} />
+        <span style={spanStyle} className="form__counter">{tweetRemainingLength}</span>
       </form>
     </section>
   )
