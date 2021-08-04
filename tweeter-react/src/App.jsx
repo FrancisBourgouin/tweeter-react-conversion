81e78e020f243444db6dc4c241336356986ae8aa
Francis Bourgouin <netbeuix@gmail.com>
2021-08-04 16:38:44 -0400

    created components

--- tweeter-react/src/App.jsx
+++ tweeter-react/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import Tweet from './components/Tweet';
+import TweetForm from './components/TweetForm';
+
+
 
 function App() {
   return (
     <div className="App">
-      Tweeter!
+      <Navigation />
+      <Profile />
+      <TweetForm />
+      <Tweet />
     </div>
   );
 }
